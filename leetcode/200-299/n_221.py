584a272ab0da5e81a39f5736eadc6013af9ec76b
ruheyun <1654033661@qq.com>
2026-04-11 21:53:03 +0800

    20260411q

---
+++ leetcode/200-299/n_221.py
@@ -0,0 +1,40 @@
+"""
+221. 最大正方形
+"""
+from typing import List
+
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        rows, columns = len(matrix), len(matrix[0])
+        if rows == 0 or columns == 0:
+            return 0
+        
+        maxSide = 0
+
+        for i in range(rows):
+            for j in range(columns):
+                if matrix[i][j] == '0':
+                    continue
+                maxSide = max(maxSide, 1)
+                currentMaxSide = min(rows - i, columns - j)
+                for k in range(1, currentMaxSide):
+                    flag = True
+                    if matrix[i + k][j + k] == '0':
+                        break
+                    for m in range(k):
+                        if matrix[i + k][j + m] == '0' or matrix[i + m][j + k] == '0':
+                            flag = False
+                            break
+                    if flag:
+                        maxSide = max(maxSide, k + 1)
+                    else:
+                        break
+
+        return maxSide ** 2
+                    
+
+
+
+        
+
