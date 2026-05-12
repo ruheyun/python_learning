d9720b7c68170aa69d8110b04ac6e030751f5e1a
ruheyun <1654033661@qq.com>
2026-05-12 16:31:51 +0800

    20260512q

---
+++ leetcode/200-299/n_200.py
@@ -0,0 +1,29 @@
+"""
+200. 岛屿数量
+"""
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        def dfs(grid, i, j):
+
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0': 
+                return
+
+            grid[i][j] = '0'
+
+            dfs(grid, i + 1, j)
+            dfs(grid, i, j + 1)
+            dfs(grid, i - 1, j)
+            dfs(grid, i, j - 1)
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(grid, i, j)
+                    count += 1
+        
+        return count
