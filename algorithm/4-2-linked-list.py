67501a02eca5602173342118ca11852cf337d68a
ruheyun <1654033661@qq.com>
2026-05-28 21:56:05 +0800

    20260528

--- algorithm/4-2-linked-list.py
+++ algorithm/4-2-linked-list.py
@@ -34,4 +34,34 @@ def remove(n0: ListNode):
     p = n0.next
     n1 = p.next
     n0.next = n1
-        
+
+
+# Access node
+def access(head: ListNode, index: int) -> ListNode | None:
+    
+    for _ in range(index):
+        if not head:
+            return None
+        head = head.next
+    return head
+
+
+# Find node
+def find(head: ListNode, target: int) -> int:
+
+    index = 0
+    while head:
+        if head.val == target:
+            return index
+        head = head.next
+        index += 1
+    return -1
+
+
+# Doubly linked list
+class DListNode:
+    
+    def __init__(self, val):
+        self.val: int = val
+        self.next: ListNode | None = None
+        self.prev: ListNode | None = None
