470ea258ddadd899ae1dbe7f2ad659595736a6b4
ruheyun <1654033661@qq.com>
2026-06-21 21:01:38 +0800

    20260621

--- leetcode/100-199/n_148.py
+++ leetcode/100-199/n_148.py
@@ -10,19 +10,35 @@ class ListNode:
         
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        res = []
+        while cur:
+            res.append(cur.val)
+            cur = cur.next
+        res.sort()
+        cur = head
+        for _, num in enumerate(res):
+            cur.val = num
+            cur = cur.next
+        return head
+    
+    def sortList1(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next: 
+            return head
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        mid, slow.next = slow.next, None
+        left, right = self.sortList2(head), self.sortList2(mid)
+        h = res =ListNode(0)
+        while left and right:
+            if left.val <= right.val: 
+                h.next, left = left, left.next
+            else:
+                h.next, right = right, right.next
+            h = h.next
+        h.next = left if left else right
+        return res.next
+    
+    def sortList2(self, head: Optional[ListNode]) -> Optional[ListNode]:
         pass
-
-
-
-lst = [4, 2, 1, 3]
-
-head = tail = ListNode(lst[0])
-for i in range(1, len(lst)):
-    node = ListNode(lst[i])
-    tail.next = node
-    tail = node
-
-sol = Solution()
-headSort = sol.sortList(head)
-for x in headSort:
-    pass
